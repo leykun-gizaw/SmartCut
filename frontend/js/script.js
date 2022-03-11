847dc785558df08a2c8617f006bd718a1c1b9d1f
Bisrat-Aregawi <bisrat_aregawi1914@yahoo.com>
2022-03-11 17:50:56 +0300

    Merge all pages to index.html & update grid of sections

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -12,13 +12,39 @@ function asideHighlight() {
   event.target.classList.add("active");
 }
 
+function changeSection() {
+  const tabsUL = event.target.parentNode.parentNode;
+  if (!event.target.classList.contains('active')) {
+    for (let i = 0; i < tabsUL.childElementCount; i++) {
+      if (tabsUL.children[i].children[0].classList.contains('active')) {
+        tabsUL.children[i].children[0].classList.remove('active');
+        tabsUL.children[i].children[0].classList.add('text-light');
+      }
+    }
+    event.target.classList.add('active');
+    const sections = document.querySelector('#sections');
+    if (event.target.innerText === 'Stock') {
+      sections.children[0].style.display = 'block';
+      sections.children[1].style.display = 'none';
+      sections.children[2].style.display = 'none';
+    } else if (event.target.innerText === 'Cut List') {
+      sections.children[0].style.display = 'none';
+      sections.children[1].style.display = 'grid';
+      sections.children[2].style.display = 'none';
+    } else if (event.target.innerText === 'Result') {
+      sections.children[0].style.display = 'none';
+      sections.children[1].style.display = 'none';
+      sections.children[2].style.display = 'block';
+    }
+  }
+}
+
 const addProject = document.querySelector("#addProject");
 addProject.addEventListener("click", () => {
   const collapseOne = document.querySelector("#collapseOne");
   const collapseInputChildren =
     collapseOne.children[0].children[0].childElementCount;
   const projectValues = [];
-
   let missing = false;
   for (let i = 0; i < collapseInputChildren - 1; i++) {
     projectValues.push(
@@ -39,8 +65,13 @@ addProject.addEventListener("click", () => {
 
 const addStockRow = document.querySelector("#addStockRow");
 addStockRow.addEventListener("click", () => {
-  console.log("ADD STOCK ROW");
   const stockTable = document.querySelector("#stockTable");
   stockTable.innerHTML +=
     '<tr><th scope="row" class="small-width">3</th><td><input type="number" class="form-control" aria-label="Recipient\'s username" aria-describedby="basic-addon2"></td><td><input type="number" class="form-control" placeholder="000" aria-label="Recipient\'s username" aria-describedby="basic-addon2"></td><td><div class="input-group" style="margin: auto;"><input type="text" class="table-input form-control" placeholder="0.00" aria-label="Recipient\'s username" aria-describedby="basic-addon2" style="width: 80px;"><select class="table-select form-select" aria-label="stckLength" style="width: 60px"><option selected>cm</option><option value="1">mm</option><option value="2">m</option></select></div></td><td><div class="input-group" style="margin: auto;"><input type="text" class="table-input form-control" placeholder="0.00" aria-label="Recipient\'s username" aria-describedby="basic-addon2" style="width: 80px;"><select class="table-select form-select" aria-label="stckLength" style = "width: 60px;"><option selected>ETB</option><option value="1">€</option><option value="2">$</option></select></div></td><td style="text-align: center"><button type="button" class="btn btn-outline-danger" onclick="rowRemove();">Delete Record</button></td></tr>';
+});
+
+const addTable = document.querySelector('#addTable');
+addTable.addEventListener('click', () => {
+  const tableSection = document.querySelector('#midSection');
+  tableSection.innerHTML += `<div class="c1_r1"><button class="btn btn-danger btn-sm hidden-table-delete"><i class="fas fa-window-close fa-sm"></i></button><table class="importsTable table table-sm table-bordered"><thead><tr><th colspan="3">Rectangular Holo Section</th></tr></thead><tbody><tr><td>No</td><td>Quantity</td><td>Length(cm)</td></tr><tr><td scope="row">1</td><td><input type="number" name="" style="width: 80px"></td><td><input type="number" name="" style="width: 80px"></td></tr><tr><td scope="row">2</td><td><input type="number" name="" style="width: 80px"></td><td><input type="number" name="" style="width: 80px"></td></tr><tr><td scope="row">3</td><td><input type="number" name="" style="width: 80px"></td><td><input type="number" name="" style="width: 80px"></td></tr></tbody></table></div>`
 });
