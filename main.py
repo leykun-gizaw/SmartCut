0364182bc6c8be993d5f6f73f6265a034f5a9d7d
Bisrat-Aregawi <bisrat_aregawi1914@yahoo.com>
2022-03-23 12:17:27 +0300

    Comment modules, classes, and functions of backend logic

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""Entry module to the backend.
+
+Defines and exposes functions to javascript by using `eel` library.
+    Typical usage example:
+
+    # Following should open a chromium window serving appropriate index.html
+    $ python main.py
+"""
 import eel
 from copy import deepcopy
 from solver.steel_model import Steel
@@ -8,8 +16,18 @@ eel.init("web")
 
 @eel.expose
 def capture_data(data):
-    """
-    Capture data from frontend and send back optimized data
+    """Capture data from frontend and send back optimized data.
+
+    Args:
+        data: Dictionary converted from JSON containing all parsed CSV data.
+            data = {
+                'steel_name': {
+                    'Lengths': [-,-,-],
+                    'Quantity': [-,-,-],
+                },
+            }
+    Returns:
+        Optimized data from solve module's `solve_ip` function.
     """
     optimized = {}
     for k, v in revise_data(data).items():
@@ -25,10 +43,17 @@ def capture_data(data):
     return optimized
 
 def revise_data(data):
+    """Refactor `data` and make it ready for data model construction
+
+    Populate `lengths` with all lengths by allowing multiple insertion of
+    identical lengths according to number of quantities for each length.
+
+    Args:
+        data: Dictionary with length & quantity lists of all steels.
+
+    Returns:
+        Optimized data from solve module's `solve_ip` function.
     """
-    Refactor `data` and make it ready for data model construction
-    """
-    global data_model
     revised_data = {}
     for k, v in data.items():
         lengths = []
--- solver/steel_model.py
+++ solver/steel_model.py
@@ -1,11 +1,35 @@
 #!/usr/bin/python3
-"""Module defines steel class"""
+"""Module defines a class `Steel` to construct data-model of its object.
+    Typical usage example:
+
+    import steel_model
+
+    # Just creating the object.
+    angle_iron = steel_model.Steel('ANGLE_IRON', [23, 32, 34, 43])
+
+    # Creating all attributes required by the solve module
+    angle_iron.create_model()
+"""
 from ortools.linear_solver import pywraplp
 
 
 class Steel:
     """
-    Class to construct steel object with methods
+    Construct steel object with methods
+
+    Class requires two mandatory arguments when initializing object; name of
+    steel (of type string) and Lengths list (of type list) containing all required
+    lengths to be cut. Its methods are responsible to create the required
+    data-model of created object ready to be sent to `solve` module.
+
+    Attributes:
+        name: String representing real life name of object.
+        Lengths: List containing all required cut lengths.
+        solver: Solver object from Google's ORTools python wrapper `pywraplp`.
+        wrapper: Wrapper for python of an ORTools solver
+        data: Dict created by `data_model` method using Lengths attribute.
+        x: Dict created by `variables` method (details in method definition).
+        y: Dict created by `variables` method (details in method definition).
     """
     def __init__(self, steel_name, Lengths):
         """
@@ -13,7 +37,11 @@ class Steel:
         """
         self.name = steel_name
         self.Lengths = Lengths
+
+        # Assign solver attribute to object for `solve` module's convenience
         self.solver = pywraplp.Solver.CreateSolver('SCIP')
+
+        # Assign wrapper attribute for the same reason mentioned above
         self.wrapper = pywraplp
         return None
 
@@ -25,12 +53,18 @@ class Steel:
         self.data['Lengths'] = self.Lengths
         self.data['cuts'] = list(range(len(self.Lengths)))
         self.data['std_pcs'] = self.data['cuts']
-        self.data['std_len'] = 6000
+        self.data['std_len'] = 6000  # 6000 in millimeters
         return None
 
     def variables(self):
-        """
-        Create appropriate variables based on data model
+        """Create appropriate variables based on data model.
+
+        x: Matrix created from the constraint coefficients.
+        x = {(0, 0): 'x_0_0', (0, 1): 'x_0_1', ...}
+        y = {0: 'y[0]', 1: 'y[1]', ...}
+
+        more info on variable definition for solver:
+            https://developers.google.com/optimization/bin/bin_packing
         """
         self.x, self.y = {}, {}
         self.data_model()
@@ -48,11 +82,14 @@ class Steel:
                 1,
                 'y[{}]'.format(j)
             )
+        print(self.y)
         return None
 
     def constraints(self):
-        """
-        Create constraints based on variables
+        """Create constraints based on variables.
+
+        more info on variable constraints definition for solver:
+            https://developers.google.com/optimization/bin/bin_packing
         """
         self.variables()
         for i in self.data['cuts']:
@@ -69,7 +106,7 @@ class Steel:
 
     def create_model(self):
         """
-        Initialize all methods to make object ready for computation
+        Call all methods to make object ready for computation
         """
         self.constraints()
         return None
