5a29b06118a017496913a73a2c3cccfea9f9b078
Bisrat-Aregawi <bisrat_aregawi1914@yahoo.com>
2022-03-24 23:58:17 +0300

    Change location of childArray function to utilities

--- web/js/aside.js
+++ web/js/aside.js
@@ -73,29 +73,6 @@ function makeProjectCard (project, customer, began, ends) {
 }
 
 /**
-  * Return a childrens array according to provided objects array.
-  * @param {Array} ...arr Spreaded arguments array
-  * @returns {Array} Array of children where each element is typeof TreeNode
-  */
-function childArray (...arr) {
-  // Define chidren array to hold all children objects
-  const children = [];
-  /**
-    * For each object in ...arr, construct TreeNode object with their element
-    * attributes given a class list and inner text and when finished appended
-    * to children array.
-    */
-  for (let i = 0; i < arr.length; i++) {
-    const obj = new TreeNode(elementMaker(arr[i].elem));
-    if (arr[i].classList !== []) obj.element.classList.add(...arr[i].classList);
-    if (arr[i].content !== '') obj.element.innerText = arr[i].content;
-    children.push(obj);
-  }
-  // Return children array
-  return children;
-}
-
-/**
   * Recursively construct DOM Tree of provided parameter. Provided parameter
   * is a tree structure containing root element and childrens array for each
   * node.
--- web/js/utilities.js
+++ web/js/utilities.js
@@ -2,9 +2,6 @@
   * aside module.
   * @module js/utilities.js
   */
-function elementMaker (elem) {
-  return document.createElement(elem);
-}
 
 /** Class representing a tree node */
 class TreeNode {
@@ -20,4 +17,30 @@ class TreeNode {
   }
 }
 
-export { elementMaker, TreeNode };
+/**
+  * Return a childrens array according to provided objects array.
+  * @param {Array} ...arr Spreaded arguments array
+  * @returns {Array} Array of children where each element is typeof TreeNode
+  */
+function childArray (...arr) {
+  // Define chidren array to hold all children objects
+  const children = [];
+  /**
+    * For each object in ...arr, construct TreeNode object with their element
+    * attributes given a class list and inner text and when finished appended
+    * to children array.
+    */
+  for (let i = 0; i < arr.length; i++) {
+    const obj = new TreeNode(elementMaker(arr[i].elem));
+    if (arr[i].classList !== []) obj.element.classList.add(...arr[i].classList);
+    if (arr[i].content !== '') obj.element.innerText = arr[i].content;
+    children.push(obj);
+  }
+  // Return children array
+  return children;
+}
+
+function elementMaker (elem) {
+  return document.createElement(elem);
+}
+export { elementMaker, TreeNode, childArray };
