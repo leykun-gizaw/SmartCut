daf8313603f8e359ed57e695f3589731e1eca82d
Bisrat-Aregawi <bisrat_aregawi1914@yahoo.com>
2022-03-24 21:40:09 +0300

    Comment utilities module functions

--- web/js/utilities.js
+++ web/js/utilities.js
@@ -1,8 +1,19 @@
+/**
+  * aside module.
+  * @module js/utilities.js
+  */
 function elementMaker (elem) {
   return document.createElement(elem);
 }
 
+/** Class representing a tree node */
 class TreeNode {
+  /**
+    * Create a node object with element and children.
+    * @property {HTMLElement} element: DOM Element represented by current
+    * object.
+    * @property {Array} children: Array of Child TreeNodes of the object.
+    */
   constructor (element) {
     this.element = element;
     this.children = [];
